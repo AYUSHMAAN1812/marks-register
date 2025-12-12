6c9de336d91a0be92f9807d746692dedbed422da
AYUSHMAAN1812 <ayushmaan2005dav@gmail.com>
2025-12-12 16:23:38 +0530

    added suspense

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -6,8 +6,10 @@ export default function DashboardLayout() {
   return (
     // Wrap the content (which is app/dashboard/page.js) in Suspense
     // This resolves the useSearchParams error during Vercel build time.
-    <Suspense>
-      <Dashboard/>
-    </Suspense>
+    <>
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <Dashboard />
+      </Suspense>
+    </>
   );
 }
